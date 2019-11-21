fbab4290741935073b20efcdb1de3412a5e5bb53
Martijn <martijn.meeldijk@student.ucll.be>
2019-11-21 18:22:36 +0100

    test gitignore

--- src/database/ArtikelSchrijver.java
+++ src/database/ArtikelSchrijver.java
@@ -1,4 +1,26 @@
 package database;
 
+import model.Artikel;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Scanner;
+
 public class ArtikelSchrijver {
+    private boolean write(String filename, HashMap<Integer, Artikel> artikels) throws FileNotFoundException {
+
+
+        File file = new File(filename);
+        Scanner scanner = new Scanner(file);
+        while(scanner.hasNextLine()){
+            Artikel artikel = new Artikel();
+            Scanner linescanner = new Scanner(scanner.nextLine());
+            linescanner.useDelimiter(",");
+
+            int code = Integer.valueOf(linescanner.next());
+            String omschrijving;
+            String artikelgroep;
+            double verkoopprijs;
+            int voorraad;
 }
