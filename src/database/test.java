e9fce46e49251dda52bf5bca67deb26b28d67353
Bram <bram.verbist@student.ucll.be>
2019-11-21 20:06:09 +0100

    test reader

--- src/database/test.java
+++ src/database/test.java
@@ -8,8 +8,8 @@ import java.util.HashMap;
 public class test {
     public static void main(String[] args) throws FileNotFoundException {
         ArtikelLezer lezer = new ArtikelLezer();
-        HashMap<Integer,Artikel> map = lezer.read("src/bestanden/artikel");
-        for(int i:map.keySet()){
+        HashMap map = lezer.read("src/bestanden/artikel");
+        for(Object i:map.keySet()){
             System.out.println(map.get(i).toString());
         }
     }
