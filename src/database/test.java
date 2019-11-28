1dce5f5ff63e3c18365b3e66fa51225c33323928
Martijn <martijn.meeldijk@student.ucll.be>
2019-11-28 15:37:15 +0100

    test geschreve

--- src/database/test.java
+++ src/database/test.java
@@ -1,14 +1,25 @@
 package database;
 
+import model.Artikel;
+
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.HashMap;
 
 public class test {
     public static void main(String[] args) throws FileNotFoundException {
         ArtikelLoadSave lezer = new ArtikelLoadSave();
-        HashMap map = lezer.read("src/bestanden/artikel");
+        lezer.setFilename("src/bestanden/artikel");
+        HashMap<Integer, Artikel> map = lezer.load();
         for(Object i:map.keySet()){
             System.out.println(map.get(i).toString());
+        }
+        map.put(20, new Artikel(20, "poep ding is niet lekker", "vieze dingen", 20, 3));
+
+        try {
+            lezer.save();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
