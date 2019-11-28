2e883fc152e7fe33e9f784f6261586bf8805f29d
Maarten <maarten-spam@hotmail.com>
2019-11-28 16:28:42 +0100

    mlkj

--- src/database/LoadSave.java
+++ src/database/LoadSave.java
@@ -5,7 +5,7 @@ import model.Artikel;
 import java.io.FileNotFoundException;
 import java.util.HashMap;
 
-public interface DbStrategie {
+public interface LoadSave {
     public HashMap<Integer, Artikel> load(String filename) throws FileNotFoundException;
     public void save();
 
