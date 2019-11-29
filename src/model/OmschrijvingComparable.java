fd2fdbe8090a5162bb8c7e080cb86bd0f9673b9b
Bram <bram.verbist@student.ucll.be>
2019-11-29 23:17:16 +0100

    comparable voor omschrijving eerst lowercase

--- src/model/OmschrijvingComparable.java
+++ src/model/OmschrijvingComparable.java
@@ -5,6 +5,6 @@ import java.util.Comparator;
 public class OmschrijvingComparable implements Comparator<Artikel> {
     @Override
     public int compare(Artikel o1, Artikel o2) {
-        return o1.getOmschrijving().compareTo(o2.getOmschrijving());
+        return o1.getOmschrijving().toLowerCase().compareTo(o2.getOmschrijving().toLowerCase());
     }
 }
