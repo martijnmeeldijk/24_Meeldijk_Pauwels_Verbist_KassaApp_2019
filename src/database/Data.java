0a9f313a82291aa6c2d83bc0ac8bdbe0998e658b
Maarten <maarten-spam@hotmail.com>
2019-12-01 15:21:35 +0100

    added data

--- src/database/Data.java
+++ src/database/Data.java
@@ -1,4 +1,22 @@
 package database;
 
+import model.Artikel;
+
+import java.util.HashMap;
+
 public class Data {
+    private ArtikelDbContext artikelDbContext;
+    private HashMap<Integer, Artikel>artikels;
+
+    public Data(ArtikelDbContext artikelDbContext) {
+        this.artikelDbContext = artikelDbContext;
+        artikels= artikelDbContext.load();
+    }
+
+    public void SaveData(){
+        artikelDbContext.save(artikels);
+    }
+
+    
+
 }
