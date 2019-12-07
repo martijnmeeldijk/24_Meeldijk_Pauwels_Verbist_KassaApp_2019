c314a0157121caec4692c2cd3d5db5f14887104e
Martijn <martijn.meeldijk@student.ucll.be>
2019-12-07 23:16:31 +0100

    Korting werd fout berekend in ticket (gefixt)

--- src/model/kasticket/decorators/KortingFooter.java
+++ src/model/kasticket/decorators/KortingFooter.java
@@ -1,6 +1,43 @@
 package model.kasticket.decorators;
 
+import controller.AantalList;
+import model.Artikel;
+import model.Winkel;
 import model.kasticket.KasTicketDecorator;
+import model.kasticket.Ticket;
+import model.korting.Korting;
 
-public class TotaalPrijsFooter extends KasTicketDecorator {
+import java.util.List;
+
+public class KortingFooter extends KasTicketDecorator {
+    Ticket ticket;
+
+    public KortingFooter(Ticket ticket, Winkel winkel) {
+        super(winkel);
+        this.ticket = ticket;
+    }
+
+    @Override
+    public String print() {
+        return ticket.print() + "\nPrijs zonder korting: €" +
+                String.format("%.2f", prijsZonderKorting()) + "\nKorting: €" + String.format("%.2f",korting());
+    }
+
+
+    private double prijsZonderKorting() {
+        double totaal = 0.0;
+        for (Artikel artikel : ticket.getWinkel().getActieveBestelling().getArtikels()) {
+            totaal += artikel.getVerkoopprijs();
+        }
+        return totaal + korting();
+    }
+
+    private double korting() {
+
+        double korting = 0.0;
+        for (Korting k : ticket.getWinkel().getKortingen()) {
+            korting += k.getKorting(AantalList.getList(ticket.getWinkel()));
+        }
+        return korting;
+    }
 }
