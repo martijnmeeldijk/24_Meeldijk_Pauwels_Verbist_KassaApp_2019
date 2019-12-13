f7b39a83dbf9c150a6b376a15632839ede429934
Maarten <maarten-spam@hotmail.com>
2019-12-13 14:38:18 +0100

    Auto stash before merge of "master" and "origin/master"
    small changes

--- src/database/LoadSaveStrat/ExcelAdapter.java
+++ src/database/LoadSaveStrat/ExcelAdapter.java
@@ -1,4 +1,5 @@
 package database.LoadSaveStrat;
+
 import excel.ExcelPlugin;
 import jxl.read.biff.BiffException;
 import jxl.write.WriteException;
@@ -13,48 +14,51 @@ public class ExcelAdapter implements LoadSaveStrategy {
     private ExcelPlugin excelPlugin;
 
     public ExcelAdapter() {
-        excelPlugin= new ExcelPlugin();
+        excelPlugin = new ExcelPlugin();
     }
 
     @Override
-        public HashMap<Integer, Artikel> load(){
-            HashMap<Integer,Artikel>artikels= new HashMap<>();
-            File file = new File("src/bestanden/artikel.xls");
-            try {
-                ArrayList<ArrayList<String>> excelLijst=excelPlugin.read(file);
-                for(ArrayList<String> artikel: excelLijst){
-                    Integer code = Integer.parseInt(artikel.get(0));
-                    Artikel artikel1= Artikel.MaakArtikel(artikel.get(0),artikel.get(2),artikel.get(1),artikel.get(3),artikel.get(4));
-                    artikels.put(code,artikel1);
-                }
-            } catch (BiffException|IOException e) {
-                e.printStackTrace();
-            }
-            return artikels;
-        }
+    public HashMap<Integer, Artikel> load() {
+        HashMap<Integer, Artikel> artikels = new HashMap<>();
 
-        private ArrayList<String> geefArray(Artikel artikel){
-        ArrayList<String>artikelInLijst= new ArrayList<>();
+        try {
+            ArrayList<ArrayList<String>> excelLijst = excelPlugin.read(new File("src/bestanden/artikel.xls"));
+            for (ArrayList<String> artikel : excelLijst) {
+                Integer code = Integer.parseInt(artikel.get(0));
+                Artikel artikel1 = Artikel.MaakArtikel(artikel.get(0), artikel.get(2), artikel.get(1), artikel.get(3), artikel.get(4));
+                artikels.put(code, artikel1);
+
+
+            }
+
+        } catch (BiffException | IOException e) {
+            e.printStackTrace();
+        }
+        return artikels;
+    }
+
+    private ArrayList<String> geefArray(Artikel artikel) {
+        ArrayList<String> artikelInLijst = new ArrayList<>();
         artikelInLijst.add(String.valueOf(artikel.getCode()));
         artikelInLijst.add(artikel.getOmschrijving());
-            artikelInLijst.add(String.valueOf(artikel.getArtikelgroep()));
-            artikelInLijst.add(String.valueOf(artikel.getVerkoopprijs()));
-            artikelInLijst.add(String.valueOf(artikel.getVoorraad()));
+        artikelInLijst.add(String.valueOf(artikel.getArtikelgroep()));
+        artikelInLijst.add(String.valueOf(artikel.getVerkoopprijs()));
+        artikelInLijst.add(String.valueOf(artikel.getVoorraad()));
 
-            return artikelInLijst;
+        return artikelInLijst;
+    }
+
+    @Override
+    public void save(HashMap<Integer, Artikel> artikels) {
+        ArrayList<ArrayList<String>> items = new ArrayList<>();
+        for (Integer key : artikels.keySet()) {
+            items.add(geefArray(artikels.get(key)));
         }
-
-        @Override
-        public void save(HashMap<Integer, Artikel> artikels) {
-            ArrayList<ArrayList<String>>items= new ArrayList<>();
-            for(Integer key: artikels.keySet()){
-                items.add(geefArray(artikels.get(key)));
-            }
-            File file = new File("src/bestanden/artikel.xls");
-            try {
-                excelPlugin.write(file,items);
-            } catch (BiffException|IOException|WriteException e) {
-                e.printStackTrace();
-            }
+        File file = new File("src/bestanden/artikel.xls");
+        try {
+            excelPlugin.write(file, items);
+        } catch (BiffException | IOException | WriteException e) {
+            e.printStackTrace();
         }
     }
+}
