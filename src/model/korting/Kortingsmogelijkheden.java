addfeb3b4d02a05764062ee0a9ffe9d9242fb173
Bram <bram.verbist@student.ucll.be>
2019-12-22 10:22:52 +0100

    commands weg

--- src/model/korting/Kortingsmogelijkheden.java
+++ src/model/korting/Kortingsmogelijkheden.java
@@ -4,12 +4,4 @@ public enum Kortingsmogelijkheden {
     Groep,
     Drempel,
     Duurst
-
-    /*private int korting;
-
-    Kortingsmogelijkheden(int korting){this.korting=korting;}
-
-    public void setKorting(int korting){this.korting=korting;}
-    public int getKorting(){return korting;}*/
-
 }
